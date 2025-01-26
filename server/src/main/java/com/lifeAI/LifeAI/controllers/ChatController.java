e1b8739716673b09fcaf17309b81762def19630e
BelisAliosmanova <benidebelis@gmail.com>
2025-01-26 13:40:29 +0200

    fix mapping

--- server/src/main/java/com/lifeAI/LifeAI/controllers/ChatController.java
+++ server/src/main/java/com/lifeAI/LifeAI/controllers/ChatController.java
@@ -4,10 +4,7 @@ import com.lifeAI.LifeAI.services.OpenAIService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -36,7 +33,7 @@ public class ChatController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/dailyReminder")
+    @GetMapping("/dailyReminder")
     public ResponseEntity<?> receiveDailyReminder() {
         return ResponseEntity.ok(openAIService.getDailyReminder());
     }
